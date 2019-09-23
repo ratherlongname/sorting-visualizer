cf281600c641827c4c466b23be0b33df047c9bd0
ratherlongname <shivam.bansal@live.com>
2019-09-23 06:10:21 +0530

    Touch relevant files

---
+++ sorting-visualizer.js
@@ -0,0 +1,7 @@
+const unsorted_arr = [14, 3, 15, 6, 2, 8, 9, 11, 1, 12, 10, 5, 4, 7, 13];
+// TODO: random arr, specific distributions, change no. of elements, specify arr
+
+
+// TODO: sort and make data array
+
+// TODO: make visual
